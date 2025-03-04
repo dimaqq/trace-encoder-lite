d2c15f6d21bf0ce6590bea3ee394b7f01e1a702d
Dima Tisnek <dimaqq@gmail.com>
2025-03-04 13:31:12 +0900

    fix: correct bytes attribute encoding, as OTEL may soon allow those

--- otlp_json/__init__.py
+++ otlp_json/__init__.py
@@ -117,9 +117,7 @@ def _value(v: _VALUE) -> dict[str, Any]:
         # FIXME: not reached!
         # The API/SDK coerces bytes to str or drops the attribute, see comment in:
         # https://github.com/open-telemetry/opentelemetry-python/issues/4118
-        return {
-            "bytesValue": bytes(v)
-        }  # FIXME this can't be right; gotta encode this somehow
+        return {"bytesValue": bytes(v).hex()}
     if isinstance(v, str):
         return {"stringValue": str(v)}
     if isinstance(v, Sequence):
